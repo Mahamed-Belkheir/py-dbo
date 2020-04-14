abdc7b4330134f5b196e13292f0f6c2c5c224e7f
mahamedbelkeir <mohamedbel@protonmail.com>
2020-04-15 01:46:19 +0200

    basic working insert sql builder implementation, TODO: implement basic CRUD sql generation

--- dbo/dialects.py
+++ dbo/dialects.py
@@ -5,6 +5,10 @@ class Dialect_Mysql:
         return f"SELECT * FROM {table}"
 
     @staticmethod
+    def insert(table, **values):
+        return f"INSERT ({','.join(values.keys())}) INTO {table} VALUES ({','.join([str(x) for x in values.values()])})"
+
+    @staticmethod
     def varchar(self, length):
         return f"VARCHAR(${length}),"
 
@@ -21,8 +25,8 @@ class Dialect_Mysql:
         return "DATETIME,"
     
     @staticmethod
-    def and_query(**kawrgs):
-        return " AND ".join([item[0]+'='+str(item[1]) for item in kawrgs.items()])
+    def and_query(**query):
+        return " WHERE (" + " AND ".join([item[0]+'='+str(item[1]) for item in query.items()]) +")"
 
 
 
--- dbo/model.py
+++ dbo/model.py
@@ -8,14 +8,16 @@ class Model(DBO):
     def find(cls, query_obj = None ,**query):
         sql = cls.sql.find(cls.__name__)
         if (query):
-            sql += f" WHERE ({cls.sql.and_query(**query)})"
+            sql += cls.sql.and_query(**query)
         elif (query_obj):
-            sql += f" WHERE ({cls.sql.and_query(**query_obj)})"
+            sql += cls.sql.and_query(**query_obj)
         return sql
 
     @classmethod
-    def insert(clf):
-        pass
+    def insert(cls, query_obj = {} ,**query):
+        sql = cls.sql.insert(cls.__name__, **query_obj, **query)
+        return sql
+        
 
     @classmethod
     def update(clf):
@@ -40,3 +42,4 @@ class Person(Model):
 
 
 print(Person.find({'name': "john"}))
+print(Person.insert(name='john', age=10))
